050e0d9287352a5be297e94a62cebd54f374edec
axelhermawan048-coder <axelhermawan048@gmail.com>
2026-09-12 21:49:36 +0700

    Update server2.js

--- server2.js
+++ server2.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // 1. KONEKSI MONGODB ATLAS
